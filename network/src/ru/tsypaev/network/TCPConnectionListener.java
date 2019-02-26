66790a5d458cd27a6b4901ee86e579437d5bd7b1
Tsypaev Vladimir <tsypaevova@yandex.ru>
2019-02-26 22:57:45 +0500

    Init project

---
+++ network/src/ru/tsypaev/network/TCPConnectionListener.java
@@ -0,0 +1,12 @@
+package ru.tsypaev.network;
+
+public interface TCPConnectionListener {
+
+    void onConnectionReady(TCPConnection tcpConnection);
+
+    void onRecieveString(TCPConnection tcpConnection, String value);
+
+    void onDisconnect(TCPConnection tcpConnection);
+
+    void onException(TCPConnection tcpConnection, Exception e);
+}
